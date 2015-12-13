9e6d926c6b0a9157c870f6a4aec64601485fbeff
Roy Segall <roy.segall@gizra.com>
2015-12-13 19:14:39 +0200

    Adding the basic work for the reports page.

--- modules/apps_entity_restrictions_reports/includes/AppsEntityRestrictionsReports.php
+++ modules/apps_entity_restrictions_reports/includes/AppsEntityRestrictionsReports.php
@@ -7,4 +7,24 @@
 
 class AppsEntityRestrictionsReports {
 
+  /**
+   * Check if the user have any permission to view reports.
+   *
+   * @param AppsEntityRestriction $app
+   *   The application instance.
+   *
+   * @param stdClass $account
+   *   The user object. Optional.
+   *
+   * @return bool
+   */
+  public static function reportsAccess(AppsEntityRestriction $app, stdClass $account = NULL) {
+    if (empty($account)) {
+      global $user;
+      $account = user_load($user->uid);
+    }
+
+    return $account->uid == $app->getUid() ? user_access('view any application reports', $account) : user_access('view owned application reports', $account);
+  }
+
 }
