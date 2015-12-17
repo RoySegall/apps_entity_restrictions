dd7c49bbfc7340b67dbe97e69ae315c0b005ad9a
Roy Segall <roy.segall@gizra.com>
2015-12-17 23:27:48 +0200

    Dray graph easily.

--- modules/apps_entity_restrictions_reports/js/AppsEntityRestrictionsReports.js
+++ modules/apps_entity_restrictions_reports/js/AppsEntityRestrictionsReports.js
@@ -5,32 +5,7 @@
    */
   Drupal.behaviors.charts = {
     attach: function (context, settings) {
-      var options = {
-        // Ticks to be used to distribute across the axis length. As this axis type relies on the index of the value rather than the value, arbitrary data that can be converted to a string can be used as ticks.
-        ticks: ['One', 'Two', 'Three'],
-        // If set to true the full width will be used to distribute the values where the last value will be at the maximum of the axis length. If false the spaces between the ticks will be evenly distributed instead.
-        stretch: true,
-        low: 0,
-        showArea: true,
-        lineSmooth: Chartist.Interpolation.simple({
-          divisor: 2
-        }),
-        axisY: {
-          onlyInteger: true,
-          offset: 20
-        }
-      };
-
-      var charts = settings.chart;
-      var days = charts.days;
-      var hits = charts.hits;
-
-      var data = {
-        labels: days,
-        series: hits
-      };
-
-      new Chartist.Line('.ct-chart', data, options);
+      Drupal.behaviors.graph.generate(settings.chart.days, settings.chart.hits);
     }
   };
 
@@ -49,28 +24,42 @@
           url: Drupal.settings.chart.basePath + '/' + month + '/' + year,
           dataType: 'json',
           success: function (matches) {
-            console.log(matches);
+            Drupal.behaviors.graph.generate(matches.days, matches.hits);
           },
           error: function(data) {
             console.log(data);
           }
         });
-
-        new Chartist.Line('.ct-chart', {
-          labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-          series: [
-            [12, 9, 7, 8, 5],
-            [2, 1, 3.5, 7, 3],
-            [1, 3, 4, 5, 6]
-          ]
-        }, {
-          fullWidth: true,
-          chartPadding: {
-            right: 40
-          }
-        });
-
       });
+    }
+  };
+
+  /**
+   * Generating graph easily.
+   */
+  Drupal.behaviors.graph = {
+    generate: function(days, hits) {
+      var data = {
+        labels: days,
+        series: hits
+      };
+
+      var options = {
+        fullWidth: true,
+        chartPadding: {
+          right: 50
+        },
+        stretch: true,
+        showArea: true,
+        lineSmooth: Chartist.Interpolation.simple({
+          divisor: 2
+        }),
+        axisY: {
+          onlyInteger: true
+        }
+      };
+
+      new Chartist.Line('.ct-chart', data, options);
     }
   };
 
